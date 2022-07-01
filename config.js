a6e4d33580682ef032f9285dc5a7d91636d8918c
Ziggy <68012775+Ziggy434@users.noreply.github.com>
2022-07-01 18:35:48 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+config = {
+
+    // Directories
+    cmdPath:"./commands/", // best not to change this
+
+
+    // Ticket Settings
+    ticketCatagory: "910350738482425896", // what catagory to create the ticket in
+    ticketLogChannel: "992366561396539394", // Where to send the ticket logs
+
+
+
+    // Bot Settings
+    prefix: "..", // You can set you're own prefix here!
+    status: "Ziggy Development Ticket Managment", // Change the bots status here
+    statusType: "PLAYING", // Change the staus type for the bot
+    token: "You're Token Here" // You're bots token
+}
+
+module.exports = { config }
